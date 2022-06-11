1ee228b660125c87ef3ab705c2a560924c096c14
zeeshanzaidi1974 <98213611+zeeshanzaidi1974@users.noreply.github.com>
2022-06-11 10:41:14 +0200

    Add files via upload

---
+++ script.js
@@ -0,0 +1,26 @@
+
+var 	acc_no  = document.getElementById("firstNumber") .value;
+
+
+
+
+
+function myFunction() {
+  alert("Hello! I am an alert box!");
+}
+
+
+function multiplyBy()
+{
+        num1 = document.getElementById("firstNumber").value;
+        num2 = document.getElementById("secondNumber").value;
+
+        document.getElementById("result").innerHTML = num1 * num2;
+}
+
+function divideBy() 
+{ 
+        num1 = document.getElementById("firstNumber").value;
+        num2 = document.getElementById("secondNumber").value;
+document.getElementById("result").innerHTML = num1 / num2;
+}
