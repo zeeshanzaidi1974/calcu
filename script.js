08de7b96de99a3d526eb2276496a7dd378b33ebd
zeeshanzaidi1974 <98213611+zeeshanzaidi1974@users.noreply.github.com>
2022-06-11 10:54:52 +0200

    Update script.js

--- script.js
+++ script.js
@@ -14,7 +14,9 @@ function multiplyBy()
 {
         num1 = document.getElementById("firstNumber").value;
         num2 = document.getElementById("secondNumber").value;
-
+        if(num1==' '){ 
+			alert ( 'account number is required' ) ;
+								}
         document.getElementById("result").innerHTML = num1 * num2;
 }
 
